047f188981b13b7588885bd61534deed779ea112
Nick Winkler <nick@Nicks-MacBook-Air.local>
2023-05-24 18:29:47 -0700

    added name and basic room support

--- index.js
+++ index.js
@@ -1,21 +1,52 @@
-import { io } from "socket.io-client";
-var socket = io();
+var socket = io('http://localhost:3000');
 
 var messages = document.getElementById('message-container');
 var form = document.getElementById('send-container');
 var input = document.getElementById('message-input');
+let roomList = document.getElementById('room-container');
+
+let personName;
+if(messages){
+  personName = prompt("What is your name?")
+};
+
+if(personName === '') {
+    alert("Enter in a valid name please");
+    while(personName ==='') personName = prompt("What is your name?");
+}
+
+if(personName) {
+    socket.emit('username', personName);
+}
+
+socket.on('username', personName => {
+    let item = document.createElement('li');
+    item.textContent = personName;
+    item.textContent += ' has joined';
+    messages.appendChild(item);
+})
+
+socket.on('room-created', room => {
+  const roomElement = document.createElement('div');
+  roomElement.innerHTML = room;
+  const roomLink = document.createElement('a');
+  roomLink.href = `/${room}`;
+  roomLink.innerHTML = 'Join';
+  roomList.append(roomElement);
+  roomList.append(roomLink); 
+})
 
 form.addEventListener('submit', function(e) {
-  e.preventDefault();
-  if (input.value) {
+    e.preventDefault();
+    if (input.value) {
     socket.emit('chat message', input.value);
     input.value = '';
-  }
+    }
 });
 
-socket.on('chat message', function(msg) {
-  var item = document.createElement('li');
-  item.textContent = msg;
-  messages.appendChild(item);
-  window.scrollTo(0, document.body.scrollHeight);
+socket.on('chat message', msg => {
+    var item = document.createElement('li');
+    item.textContent = `${msg}`;
+    messages.appendChild(item);
+    window.scrollTo(0, document.body.scrollHeight);
 });
--- server.js
+++ server.js
@@ -3,15 +3,46 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server);
+// Allow any origin for testing purposes. This should be changed on production
+const io = new Server(server, 
+  {cors: { origin: '*', 
+}},);
 
 const port = process.env.PORT || 3000;
 
+//body parser for form to create new rooms
+app.use(express.urlencoded({extended: true}));
+
+//setting up our folder to serve static files like our CSS style
+app.use(express.static(__dirname));
+//setting our view engine
+app.set('view engine', 'ejs');
+
+const rooms = { room: {} };
+
 app.get('/', (req, res) => {
     // res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.sendFile(__dirname + '/index.html');
+    res.render('index', { rooms: rooms });
 });
 
+app.post('/room', (req, res) => {
+
+  if(rooms[req.body.room] != null) {
+    return res.redirect('/');
+  }
+  rooms[req.body.room] = { users: {} };
+  res.redirect(req.body.room);
+  //send message that new room was created
+
+  io.emit('room-created', req.body.room);
+})
+
+app.get('/:room', (req, res) => {
+
+    res.render('room', {roomName: req.params.room})
+})
+
+//socket.io server
 io.on('connection', (socket) => {
     console.log('a user connected');
     socket.on('disconnect', () => {
@@ -19,12 +50,20 @@ io.on('connection', (socket) => {
     });
 
     socket.on('chat message', (msg) => {
-        console.log(`${msg}`);
         io.emit('chat message', msg);
+    })
+
+    socket.on('username', name => {
+      io.emit('username', name);
     })
 
   });
 
-server.listen(port, () => {
+
+server.listen( port, () => {
   console.log('listening on port 3000');
 });
+//let other devices on the network connect with that hostname (ip address)
+// server.listen( port, '192.168.1.18', () => {
+//   console.log('listening on port 3000');
+// });
